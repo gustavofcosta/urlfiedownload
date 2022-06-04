a51f0614d40c14abc8b3cb047b226e6ae51537aa
Gustavo Fernandes Costa <84602100+gustavofcosta@users.noreply.github.com>
2022-06-04 13:55:56 -0300

    commit

--- urldownload/components/Button.tsx
+++ urldownload/components/Button.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 interface Props {
     handleDownload: any
@@ -8,11 +7,11 @@ interface Props {
 export default function Button({ handleDownload, download }: Props) {
 
     return (
-        <div>
-            <button
-                onClick={handleDownload}
-                className="group w-full bg-amber-500 h-12 text-white font-semibold mt-5 rounded-md cursor-pointer hover:text-lg">{download}
-            </button>
-        </div>
+
+        <button
+            onClick={handleDownload}
+            className="group w-full bg-amber-500 h-12 text-white font-semibold mt-5 rounded-md cursor-pointer hover:text-lg">{download}
+        </button>
+
     )
 }
--- urldownload/pages/index.tsx
+++ urldownload/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import React, { useState } from 'react'
-import Button from '../components/button'
+import Button from '../components/Button'
+
 
 const Home: NextPage = () => {
 
